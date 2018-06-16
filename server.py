31dd0cf8ad69d5212bb5f1375e06ccdf939f7df2
Artem010 <cmert.123@yandex.ru>
2018-06-16 20:42:52 +0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,60 @@
+import socket, time
+
+ip = 'localhost';
+port = 9390
+cl=[]
+users =[]
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((ip,port))
+# sock.listen(3)
+
+def sendALL(addr, udata, add=False):
+	for c in users:
+			if c['_addr'] != addr:
+				for _nm in users:
+					if _nm['_addr'] == addr:
+						nm = _nm['_name']
+				nm = nm + ': ' + udata
+				if not add:
+					print('[', nm, ']', time.strftime('%X'))
+				nm = nm.encode('utf-8')
+				sock.sendto(nm, c['_addr'])
+# try:
+while 1:
+	try:
+		data, addr = sock.recvfrom(1024)
+	except:
+		for n in users:
+			if n['_addr'] == addr:
+				print(n['_name'] + 'disconnected...')
+		# print ('Disconnected' + c['_name'])
+	udata = data.decode('utf-8')
+	if not addr in cl:
+		cl.append(addr)
+		# clients.append(dict.fromkeys(['_name'], udata))
+		users.append({'_name':udata, '_addr': addr})
+		# clients.append(dict(_name=data, _addr=addr))
+		print(udata + ' join from: ' + str(addr[0].replace(' ', '')) + ':' + str(addr[1]).replace(' ', '') + ' [' + time.strftime('%X') + ']')
+		sendALL(addr, 'join from chat', True)
+	else:
+		if not data: break
+		if udata == '/online':
+			sock.sendto(str(len(users)).encode('utf-8'), addr)
+		else:
+			udata = udata.upper()
+			# udata = udata.encode('utf-8')
+			sendALL(addr, udata)
+# except socket.error as err:
+# 	print(err)
+sock.close()
+
+# [
+
+# 			{'Name': 'bob',
+# 			 'addr': [123.4324.4324, 888]},
+# 			{'Name': 'bob',
+# 			 'addr': [123.4324.4324, 888]},
+# 			{'Name': 'bob',
+# 			 'addr': [123.4324.4324, 888]}
+
+# 			]
